ef7300d94be80f05392fa0af86b586d5b23290cf
pulle12 <psummerauer@tsn.at>
2025-12-07 23:03:15 +0100

    Weiter arbeiten am Webscraper laut Youtube-Anleitung

--- scraper.py
+++ scraper.py
@@ -1,6 +1,17 @@
+import requests
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+
 print('Erster Webscraper')
 
 # https://www.fis-ski.com/DB/alpine-skiing/calendar-results.html?eventselection=&place=&sectorcode=AL&seasoncode=2026&categorycode=WC&disciplinecode=&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2026&saveselection=-1&seasonselection=
 
 baseURL = "https://www.fis-ski.com"
 URL = baseURL + "/DB/alpine-skiing/calendar-results.html?eventselection=&place=&sectorcode=AL&seasoncode=2026&categorycode=WC&disciplinecode=&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2026&saveselection=-1&seasonselection="
+
+print('Scrapen von: ' + URL)
+
+session1 = HTMLSession()
+page1 = session1.get(URL)
+page1.html.render()
+print(page1.html.html)
