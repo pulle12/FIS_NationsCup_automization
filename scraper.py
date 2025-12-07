cfca6373e34d9b8119dc78572d1498aa8dbe5e28
pulle12 <psummerauer@tsn.at>
2025-12-07 22:57:08 +0100

    Beginnen mit dem Projekt. Einfügen und Aufteilen der URL

---
+++ scraper.py
@@ -0,0 +1,6 @@
+print('Erster Webscraper')
+
+# https://www.fis-ski.com/DB/alpine-skiing/calendar-results.html?eventselection=&place=&sectorcode=AL&seasoncode=2026&categorycode=WC&disciplinecode=&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2026&saveselection=-1&seasonselection=
+
+baseURL = "https://www.fis-ski.com"
+URL = baseURL + "/DB/alpine-skiing/calendar-results.html?eventselection=&place=&sectorcode=AL&seasoncode=2026&categorycode=WC&disciplinecode=&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2026&saveselection=-1&seasonselection="
