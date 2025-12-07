b26a57e194dd664af6f0679af77034958b298bdf
pulle12 <psummerauer@tsn.at>
2025-12-07 23:05:14 +0100

    Umbauen des Projektes von requests_html auf Playwright nach ChatGPT-Anleitung

--- scraper.py
+++ scraper.py
@@ -1,17 +1,18 @@
-import requests
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
 print('Erster Webscraper')
-
-# https://www.fis-ski.com/DB/alpine-skiing/calendar-results.html?eventselection=&place=&sectorcode=AL&seasoncode=2026&categorycode=WC&disciplinecode=&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2026&saveselection=-1&seasonselection=
 
 baseURL = "https://www.fis-ski.com"
 URL = baseURL + "/DB/alpine-skiing/calendar-results.html?eventselection=&place=&sectorcode=AL&seasoncode=2026&categorycode=WC&disciplinecode=&gendercode=&racedate=&racecodex=&nationcode=&seasonmonth=X-2026&saveselection=-1&seasonselection="
 
 print('Scrapen von: ' + URL)
 
-session1 = HTMLSession()
-page1 = session1.get(URL)
-page1.html.render()
-print(page1.html.html)
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto(URL)
+    
+    html = page.content()
+    print(html)
+
+    browser.close()
