a8dd8824a835a87b62385f74daf39e6ee03180e9
pulle12 <psummerauer@tsn.at>
2025-12-08 12:55:45 +0100

    Die batch-Datei aktualisieren. Testdaten (Rennen in der Zukunft einfügen um zu schauen ob einfach 0 Punkte dazukommen

--- automate.bat
+++ automate.bat
@@ -2,6 +2,7 @@
 title Ski Alpin Nationencup 25/26 Wallpaper Engine
 echo Starte Wallpaper Engine...
 
+python "C:\Users\summe\Documents\IT\Projekt_Ski\scraper.py"
 python "C:\Users\summe\Documents\IT\Projekt_Ski\ski_nationencup_25-26.py"
 
 pause
